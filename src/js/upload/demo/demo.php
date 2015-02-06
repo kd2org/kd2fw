79dd9c5c0ebeeebb75075921db6ecf61637a5968
bohwaz <>
2015-02-06 03:54:58 +0000

    Simple and powerful upload helper in JS
    FossilOrigin-Name: bba48cd27f6dbd8915414d9f0d34f401e484227e

---
+++ src/js/upload/demo/demo.php
@@ -0,0 +1,179 @@
+<?php
+
+define('STORAGE_DIR', __DIR__);
+
+define('MAX_UPLOAD_SIZE', min([
+    return_bytes(ini_get('upload_max_filesize')),
+    return_bytes(ini_get('post_max_size')),
+    return_bytes(ini_get('memory_limit'))
+]));
+
+$nb = 0;
+
+class FileManager
+{
+	protected $files = [];
+
+	public function __construct()
+	{
+		// Fix _FILES array as a multidimensional array
+		if (!empty($_FILES))
+		{
+			$this->files = $_FILES;
+
+			foreach ($this->files as $field_name=>&$value)
+			{
+				if (is_array($value['name']))
+				{
+					$new = [];
+
+					foreach ($value as $key => $all)
+					{
+						foreach ($all as $i => $val)
+						{
+							$new[$i][$key] = $val;   
+						}   
+					}
+
+					$value = $new;
+				}
+			}
+		}
+	}
+
+	public function store($file)
+	{
+		if (empty($file['size']) || empty($file['tmp_name']) || !empty($file['error']))
+		{
+			return $file['error'];
+		}
+
+		$path = STORAGE_DIR . '/upload_' . sha1_file($file['tmp_name']);
+		
+		if (file_exists($path))
+			return true;
+
+		return move_uploaded_file($file['tmp_name'], $path);
+	}
+
+	public function storeAll()
+	{
+		$i = 0;
+
+		foreach ($this->files['myFile'] as $file)
+		{
+			if (!$this->store($file))
+			{
+				return false;
+			}
+			$i++;
+		}
+
+		return $i;
+	}
+
+	public function check($hash)
+	{
+		if (!preg_match('/^[a-f0-9]+$/', $hash))
+		{
+			throw new \RuntimeException($hash . ' is not a valid SHA1 hash');
+		}
+
+		$path = STORAGE_DIR . '/upload_' . $hash;
+		return file_exists($path);
+	}
+}
+
+$fm = new FileManager;
+
+function return_bytes ($size_str)
+{
+    switch (substr($size_str, -1))
+    {
+        case 'G': case 'g': return (int)$size_str * pow(1024, 3);
+        case 'M': case 'm': return (int)$size_str * pow(1024, 2);
+        case 'K': case 'k': return (int)$size_str * 1024;
+        default: return $size_str;
+    }
+}
+
+// Hash check
+if (!empty($_POST['uploadHelper_hashCheck']) && is_array($_POST['uploadHelper_hashCheck']))
+{
+	$exist = [];
+
+	foreach ($_POST['uploadHelper_hashCheck'] as $hash)
+	{
+		if ($fm->check($hash))
+		{
+			$exist[$hash] = true;
+		}
+	}
+
+	$exist['redirect'] = './demo.php?already_ok';
+
+	echo json_encode($exist);
+	exit;
+}
+
+if (!empty($_POST))
+{
+	if (isset($_POST['uploadHelper_status']))
+	{
+		$status = (int) $_POST['uploadHelper_status'];
+
+		if (!$fm->storeAll())
+		{
+			$return = ['error' => 'Storage error.'];
+		}
+		else if ($status == 0)
+		{
+			$return = ['redirect' => './demo.php?ok'];
+		}
+		else
+		{
+			$return = ['next' => true];
+		}
+
+		echo json_encode($return);
+		exit;
+	}
+	else if (!empty($_FILES))
+	{
+		$nb = $fm->storeAll();
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Upload.js demo</title>
+	<script type="text/javascript" src="../lib.upload.js"></script>
+</head>
+
+<body>
+<?php if ($nb): ?>
+	<h2><?=$nb?> files uploaded without the upload helper</h2>
+<?php elseif (isset($_GET['ok'])): ?>
+	<h2>Upload was completed using upload helper!</h2>
+<?php elseif (isset($_GET['already_ok'])): ?>
+	<h2>All files were already uploaded!</h2>
+<?php endif; ?>
+<form method="post" enctype="multipart/form-data" action="demo.php">
+    <fieldset>
+        <legend>Upload a file</legend>
+        <input type="hidden" name="MAX_FILE_SIZE" value="<?=MAX_UPLOAD_SIZE?>" />
+        <p>Your name : <input type="text" name="myName" value="Calvin Hobbes" /></p>
+        <p><input type="file" name="myFile[]" id="myFile" multiple data-hash-check /></p>
+        <p><input type="submit" name="submit" value="Upload" /></p>
+    </fieldset>
+</form>
+
+<script>
+uploadHelper(document.forms[0].myFile);
+</script>
+
+</body>
+</html>
