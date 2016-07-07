9ead585329dadb6ab9b116ad0cfc7377239a39e2
bohwaz <>
2016-07-07 05:21:20 +0000

    Simpler utf8 check
    FossilOrigin-Name: fe016c951b0822762849550588b6b87031dbe122

--- src/lib/kd2/FeedParser.php
+++ src/lib/kd2/FeedParser.php
@@ -304,15 +304,8 @@ class FeedParser
 
     static protected function utf8_encode($str)
     {
-        if (!preg_match('%(?:
-            [\xC2-\xDF][\x80-\xBF]        # non-overlong 2-byte
-            |\xE0[\xA0-\xBF][\x80-\xBF]               # excluding overlongs
-            |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}      # straight 3-byte
-            |\xED[\x80-\x9F][\x80-\xBF]               # excluding surrogates
-            |\xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
-            |[\xF1-\xF3][\x80-\xBF]{3}                  # planes 4-15
-            |\xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
-            )+%xs', $str))
+		// Check if string is already UTF-8 encoded or not
+		if (!preg_match('//u', $str))
         {
             return utf8_encode($str);
         }
