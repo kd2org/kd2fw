d028e2dc2094d8a817de582dcec853ce58447ec6
bohwaz <>
2016-07-14 05:27:45 +0000

    Use errormanager by default in tests
    FossilOrigin-Name: 85d45e59c0b72d0b5c22e487d668c9047193614b

--- src/tests/_assert.php
+++ src/tests/_assert.php
@@ -4,6 +4,21 @@ define('KD2FW_ROOT', __DIR__ . '/../lib/kd2');
 define('DATA_DIR', __DIR__ . '/data');
 
 error_reporting(-1);
+ini_set('display_errors', 1);
+
+function __autoload($class)
+{
+	$class = explode('\\', $class);
+	$class = array_pop($class);
+	$path = KD2FW_ROOT . '/' . $class . '.php';
+
+	require $path;
+}
+
+use KD2\ErrorManager as EM;
+
+EM::setLogFile(__DIR__ . '/error.log');
+EM::enable();
 
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_WARNING, 0);
