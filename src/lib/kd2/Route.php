348120229680b18c3ce9bef0312c46d7b4d8194e
bohwaz <>
2016-07-22 02:30:32 +0000

    Route: fix brackets in arguments, add fallback and setStatus methods
    FossilOrigin-Name: d1f8892a69771a003d9ebadea6e26989ab99db28

--- src/lib/kd2/Route.php
+++ src/lib/kd2/Route.php
@@ -41,6 +41,12 @@ class RouteException extends \RuntimeException {}
 class Route
 {
 	/**
+	 * Set to true when one of the routes returns true
+	 * @var null
+	 */
+	static protected $routed = null;
+
+	/**
 	 * Known HTTP request methods
 	 * @var array
 	 */
@@ -58,6 +64,68 @@ class Route
 		'ORDERPATCH',
 		// RFC 3744
 		'ACL',
+	];
+
+	/**
+	 * List of HTTP codes and messages
+	 * @var array
+	 */
+	static protected $http_codes = [
+		100 => 'Continue',
+		101 => 'Switching Protocols',
+		102 => 'Processing',
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+		207 => 'Multi-Status',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		306 => 'Switch Proxy',
+		307 => 'Temporary Redirect',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		418 => 'I\'m a teapot',
+		422 => 'Unprocessable Entity',
+		423 => 'Locked',
+		424 => 'Failed Dependency',
+		425 => 'Unordered Collection',
+		426 => 'Upgrade Required',
+		449 => 'Retry With',
+		450 => 'Blocked by Windows Parental Controls',
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported',
+		506 => 'Variant Also Negotiates',
+		507 => 'Insufficient Storage',
+		509 => 'Bandwidth Limit Exceeded',
+		510 => 'Not Extended'
 	];
 
 	/**
@@ -166,13 +234,12 @@ class Route
 	 * @param  string   $pattern  	Regular expression to match to execute this route
 	 * @param  Callable $callback 	Route callback, called if $pattern is matching $path
 	 * Any capturing pattern will be passed to callback: $callback(capture1, capture2...)
-	 * @return mixed				TRUE if the route matched, FALSE if not
+	 * @return boolean				TRUE if the route matched, FALSE if not
 	 */
 	static public function route($path, $pattern, Callable $callback)
 	{
-		// Allow for {id}, {id_bis?}, {id:\d+}, {login?:(?i:\w+\.\w+\d+)}
-		$name_pattern = '(\w+(?:_\w+)*)(\?)?(?:\:(.+?))?';
-		$replace_pattern = sprintf('#\{%s\}#', $name_pattern);
+		// Allow for {id}, {id_bis?}, {id:\d+}, {login?:(?i:\w{2}\.\w+\d+)}
+		$replace_pattern = '#(?<!\\)\{(\w+(?:_\w+)*)(\?)?(?:\:((?:[^{}]|(?R))*?))?\}#i';
 
 		// Make a real regexp
 		$pattern = preg_replace_callback($replace_pattern, function($match) {
@@ -186,10 +253,43 @@ class Route
 		if (preg_match($pattern, self::requestURI(true), $match))
 		{
 			unset($match[0]);
-			call_user_func_array($callback, $match);
-			return true;
+			self::$routed = call_user_func_array($callback, $match);
+			return self::$routed;
 		}
 
 		return false;
+	}
+
+	/**
+	 * Will be called if no route has been successful
+	 * @param  Callable $callback Route callback
+	 * @return boolean
+	 */
+	static public function fallback(Callable $callback)
+	{
+		if (!self::$routed)
+		{
+			return call_user_func($callback);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Sets the HTTP header status
+	 * @param integer $status  Status code
+	 */
+	static public function setStatus($status)
+	{
+		$status = (int)$status;
+
+		if (!array_key_exists($status, self::$messages))
+		{
+			throw new \InvalidArgumentException('Invalid HTTP code: ' . $status);
+		}
+
+		$message = self::$http_codes[$status];
+
+	    return header($_SERVER['SERVER_PROTOCOL'] . ' ' . $status . ' ' . $message, true, $status);
 	}
 }
