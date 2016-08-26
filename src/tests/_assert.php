6170bdbb93a2b9a01f0d88797e0201989a491ad1
bohwaz <>
2016-08-26 04:16:39 +0000

    Better assertion handling
    FossilOrigin-Name: 0fc66df749b32a1034aafb5aed4fc072487f7d3d

--- src/tests/_assert.php
+++ src/tests/_assert.php
@@ -1,10 +1,9 @@
 <?php
 
+use KD2\ErrorManager as EM;
+
 define('KD2FW_ROOT', __DIR__ . '/../lib/kd2');
 define('DATA_DIR', __DIR__ . '/data');
-
-error_reporting(-1);
-ini_set('display_errors', 1);
 
 function __autoload($class)
 {
@@ -12,30 +11,18 @@ function __autoload($class)
 	$class = array_pop($class);
 	$path = KD2FW_ROOT . '/' . $class . '.php';
 
-	require $path;
+	require_once $path;
 }
 
-use KD2\ErrorManager as EM;
+@unlink(__DIR__ . '/error.log');
 
 EM::setLogFile(__DIR__ . '/error.log');
 EM::enable();
 
-assert_options(ASSERT_ACTIVE, 1);
-assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_QUIET_EVAL, 0);
-assert_options(ASSERT_CALLBACK, 'assert_fail');
-
-function test($assertion, $desc)
-{
-	assert($assertion . ' // ' . $desc . "\n");
-}
-
-function assert_fail($file, $line, $code, $desc = null)
-{
-	if (is_null($desc))
+// Check error log at shutdown
+register_shutdown_function(function () {
+	if (file_exists(__DIR__ . '/error.log'))
 	{
-		list($code, $desc) = explode('//', $code);
+		echo '[FAIL] Error log is not empty: ' . __DIR__ . '/error.log' . PHP_EOL;
 	}
-
-    echo '[FAIL] ' . $file . ':' . $line . ': ' . trim($desc) . "\n";
-}
+});
