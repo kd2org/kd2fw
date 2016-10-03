f2559597e39e4ccb04f9d3fc4cf3ec3887f7eb6f
bohwaz <>
2016-10-03 04:14:33 +0000

    Translate: Fix extract strings script
    FossilOrigin-Name: 34301092ed9bb9ac951786499ee9e59f9386ba07

--- src/tools/extract-gettext-strings.sh
+++ src/tools/extract-gettext-strings.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-([ "$1" = "" ] || [ ! -d "$1"] || [ "$2" = "" ] || [ ! -d "$2" ]) && echo "Usage: $0 LOCALES_DIRECTORY SOURCE_DIRECTORY" && exit 1
+( [ "$1" = "" ] || [ ! -d "$1" ] || [ "$2" = "" ] || [ ! -d "$2" ] ) && (echo "Usage: $0 LOCALES_DIRECTORY SOURCE_DIRECTORY" ; exit 1)
 
 
-for LANG in `find "$1" -max-depth 1 -type d -not -name '.'`
+for LANG in `find "$1" -mindepth 1 -maxdepth 1 -type d -not -name '.'`
 do
 	mkdir -p $LANG/LC_MESSAGES
 
-	find "$2" -iname '*.php*' | xargs xgettext --from-code=utf-8 -o $LANG/messages.po \
-		-L PHP -j -a -c -kKD2\gettext -kKD2\ngettext -kKD2\dgettext -kKD2\dngettext -kKD2\dcngettext \
-		-kKD2\dpgettext -kKD2\dcpgettext -kKD2\pgettext -kKD2\dcnpgettext -kKD2\npgettext \
-		-kKD2\_ -k"Translate::string" -k"Translate::plural"
+	find "$2" -iname '*.php*' -type f | xargs xgettext \
+		--from-code=utf-8 -p $LANG/LC_MESSAGES -L PHP -c \
+		-k"string:1,4c" -k"plural:1,2,6c" \
+		-k"pgettext:1c,2" -k"dpgettext:2c,3" -k"dcpgettext:2c,3" \
+		-k"npgettext:1c,2,3" -k"dnpgettext:2c,3,4" -k"dcnpgettext:2c,3,4"
 done
