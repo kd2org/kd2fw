6e056de6f6bb0906858a3f64743005080b05c6a8
bohwaz <>
2017-06-08 02:34:47 +0000

    Helpers: add shorthandmatch method
    FossilOrigin-Name: fa1926b3efb6b472f125d50a9e17cb311b2fde3c

--- src/lib/kd2/Helpers.php
+++ src/lib/kd2/Helpers.php
@@ -61,4 +61,47 @@ class Helpers
 
 		return $src;
 	}
+
+	/**
+	 * Tries to match a shorthand name to a complete name from a list
+	 * of possible names (useful for CLI scripts, like fossil!)
+	 *
+	 * @param  string $name    Name to match
+	 * @param  array  $matches List of possible matches
+	 * @return mixed FALSE if no match is found, a string if only one possible match exists, and an array if multiple matches are possible
+	 */
+	static public function shorthandMatch($name, Array $matches)
+	{
+		$name = strtolower($name);
+
+		$possibles = [];
+
+		foreach ($matches as $match)
+		{
+			if ($match == $name)
+			{
+				return $match;
+			}
+
+			if (strpos($match, $name) === 0)
+			{
+				$possibles[] = $match;
+			}
+		}
+
+		$count = count($possibles);
+
+		if ($count == 0)
+		{
+			return false;
+		}
+		elseif ($count == 1)
+		{
+			return $possibles[0];
+		}
+		else
+		{
+			return $possibles;
+		}
+	}
 }
