195cda52bc93c0420ef4c2a6a7c5c1a213bb901b
bohwaz <>
2018-01-18 02:06:02 +0000

    preg_match is not enough here
    FossilOrigin-Name: fa8f6c66ddf4d33591e953a2ebdee52b252c3ca9

--- src/lib/KD2/Mail_Message.php
+++ src/lib/KD2/Mail_Message.php
@@ -610,7 +610,7 @@ class Mail_Message
             return $value;
 		}
 
-		if (preg_match('//u', $value)) // will detect if it is a valid UTF-8 string
+		if (!$this->is_utf8($value))
 		{
 			$value = '=?UTF-8?B?'.base64_encode($value).'?=';
 		}
@@ -896,5 +896,18 @@ class Mail_Message
 		}
 
 		return $str;
+	}
+
+	public function is_utf8($str)
+	{
+		return preg_match('%(?:
+			[\xC2-\xDF][\x80-\xBF]        # non-overlong 2-byte
+			|\xE0[\xA0-\xBF][\x80-\xBF]               # excluding overlongs
+			|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}      # straight 3-byte
+			|\xED[\x80-\x9F][\x80-\xBF]               # excluding surrogates
+			|\xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
+			|[\xF1-\xF3][\x80-\xBF]{3}                  # planes 4-15
+			|\xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
+			)+%xs', $str);
 	}
 }
