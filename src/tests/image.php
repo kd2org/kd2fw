fd4edb40f149198fbdfc89388412c880de5f2cfb
bohwaz <>
2019-12-19 15:51:02 +0000

    Update tests
    FossilOrigin-Name: 1ae69a601e6116ad3d46467b0e67699cf0e1df51

--- src/tests/image.php
+++ src/tests/image.php
@@ -1,6 +1,6 @@
 <?php
 
-use KD2\Image;
+use KD2\Graphics\Image;
 use KD2\Test;
 
 require __DIR__ . '/_assert.php';
@@ -28,23 +28,16 @@ foreach ($images as $name => $size)
 
 	foreach ($libs as $lib)
 	{
-		test_image('data/images/' . $name, $lib, $size[0], $size[1], $size[2]);
+		test_resize('data/images/' . $name, $lib, $size[0], $size[1]);
+		test_rotate('data/images/' . $name, $lib, $size[2]);
+		test_crop('data/images/' . $name, $lib, $size[1], $size[2]);
 	}
 }
 
-function test_image($src, $lib, $w, $h, $o)
-{
+function test_rotate($src, $lib, $o) {
 	$im = new Image($src, $lib);
 
-	Test::equals($w, $im->width);
-	Test::equals($h, $im->height);
-
 	Test::equals($o, $im->getOrientation());
-
-	Test::assert($im->resize(200, 200, true) instanceof Image);
-
-	Test::equals(200, $im->width);
-	Test::equals(200, $im->height);	
 
 	if ($lib != 'epeg')
 	{
@@ -54,15 +47,39 @@ function test_image($src, $lib, $w, $h, $o)
 		}
 
 		Test::assert($im->flip() instanceof Image);
-		Test::assert($im->flip() instanceof Image);
-
-		Test::assert($im->crop(100, 100) instanceof Image);
-		Test::equals(100, $im->width);
-		Test::equals(100, $im->height);	
 	}
+}
+
+function test_resize($src, $lib, $w, $h)
+{
+	$im = new Image($src, $lib);
+
+	Test::equals($w, $im->width);
+	Test::equals($h, $im->height);
+
+	Test::assert($im->resize(200, 200, true) instanceof Image);
+
+	Test::equals(200, $im->width);
+	Test::equals(200, $im->height);
 
 	$dest = sys_get_temp_dir() . DIRECTORY_SEPARATOR . sprintf('imtest_%s_%s.%s', md5($src), $lib, substr($src, -3));
 	Test::equals(true, $im->save($dest));
 
 	//unlink($dest);
+
+	unset($im);
+}
+
+function test_crop($src, $lib)
+{
+	if ($lib == 'epeg')
+	{
+		return;
+	}
+
+	$im = new Image($src, $lib);
+	Test::assert($im->crop(100, 100) instanceof Image);
+	Test::equals(100, $im->width);
+	Test::equals(100, $im->height);
+
 }
