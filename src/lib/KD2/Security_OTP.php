7311f80ff99099cf4956f999ca39f7a6467686c6
bohwaz <>
2022-03-04 21:57:50 +0000

    Fix OTP issues with code beginning with a zero
    FossilOrigin-Name: 3f75a9f2ebc7e7ff31f9e51728cc7b2cec1076a6

--- src/lib/KD2/Security_OTP.php
+++ src/lib/KD2/Security_OTP.php
@@ -108,7 +108,8 @@ class Security_OTP
 			(ord($hmac[$offset + 2]) & 0xFF) << 8 |
 			(ord($hmac[$offset + 3]) & 0xFF);
 
-		return (string) ($code % pow(10, $digits));
+		$pattern = sprintf('%%%02dd', $digits); // eg. %06d
+		return (string) sprintf($pattern, ($code % pow(10, $digits)));
 	}
 
 	/**
@@ -166,7 +167,7 @@ class Security_OTP
 
 			for ($i = $start; $i <= $end; $i++)
 			{
-				if (hash_equals(self::HOTP($secret, $i, null, $digits, $digest), $code))
+				if (hash_equals(self::HOTP($secret, $i, null, $digits, $digest), (string) $code))
 				{
 					return true;
 				}
