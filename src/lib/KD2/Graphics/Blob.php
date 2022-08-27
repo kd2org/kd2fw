488061b2641b8660257553b2fe45fab31e43464a
bohwaz <>
2022-08-27 14:23:02 +0000

    Graphics/Blob: get a quick method to get image type
    FossilOrigin-Name: 48026523bfa166fb434d8ee622c776385cf45d32

--- src/lib/KD2/Graphics/Blob.php
+++ src/lib/KD2/Graphics/Blob.php
@@ -48,6 +48,23 @@ class Blob
 		return false;
 	}
 
+	static public function getType($data)
+	{
+		if (substr($data, 0, 3) == "\xff\xd8\xff") {
+			return 'image/jpeg';
+		}
+		elseif ($info = self::getSize($data)) {
+			return $info[2];
+		}
+		if (substr($data, 0, 4) == 'RIFF'
+			&& ctype_digit(substr($data, 4, 4))
+			&& substr($data, 8, 4) == 'WEBP') {
+			return ['image/webp'];
+		}
+
+		return null;
+	}
+
 	/**
 	 * In case we need to read directly from the file
 	 * @return string Binary blob
