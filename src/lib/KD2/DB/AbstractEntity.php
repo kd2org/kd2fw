c44b658f6f647c750310d911699916b58e01f355
bohwaz <>
2023-09-15 13:00:37 +0000

    Move out Date object
    FossilOrigin-Name: f36758a20dd0270593eae0a2d44490bd6d19eec6

--- src/lib/KD2/DB/AbstractEntity.php
+++ src/lib/KD2/DB/AbstractEntity.php
@@ -31,32 +31,6 @@ namespace KD2\DB;
  * @license AGPLv3
  */
 
-/**
- * Just a helper that tells us that the date should be stored as Y-m-d that's all
- */
-class Date extends \DateTime {
-	// For PHP 7.4
-	static public function createFromInterface(\DateTimeInterface $object): \DateTime
-	{
-		$n = new self;
-		$n->setTimestamp($object->getTimestamp());
-		$n->setTimezone($object->getTimeZone());
-		return $n;
-	}
-
-	#[\ReturnTypeWillChange]
-	static public function createFromFormat($format, $datetime, ?\DateTimeZone $object = null)
-	{
-		$v = parent::createFromFormat($format, $datetime, $object);
-
-		if (!$v) {
-			return $v;
-		}
-
-		return self::createFromInterface($v);
-	}
-}
-
 abstract class AbstractEntity
 {
 	protected $_exists = false;
