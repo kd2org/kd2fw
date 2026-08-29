d4f7a625f8ba563e849de237455e0f0569eaf1f3
bohwaz <>
2026-08-29 15:24:51 +0000

    WebDAV: various fixes for NextCloud
    FossilOrigin-Name: 5605fdf176390c90a680fa33d6c2303ac1a8ac9b

--- src/lib/KD2/WebDAV/NextCloud.php
+++ src/lib/KD2/WebDAV/NextCloud.php
@@ -429,7 +429,12 @@ abstract class NextCloud
 			$this->server->log("NC => Body:\n%s", $json);
 		}
 		elseif (is_string($v)) {
-			http_response_code(200);
+			// If response code hasn't already been set by the route function,
+			// http_response_code will return 200 if no code has been set
+			if (http_response_code() === 200) {
+				http_response_code(200);
+			}
+
 			header('Content-Type: application/json', true);
 			echo $v;
 			$this->server->log("NC => Body:\n%s", $v);
@@ -598,7 +603,7 @@ abstract class NextCloud
 			'version' => [
 				'major' => (int)$v[0],
 				'minor' => (int)$v[1],
-				'micro' => (int)$v[1],
+				'micro' => (int)$v[2],
 				'string' => self::NC_VERSION,
 				'edition' => '',
 				'extendedSupport' => false,
